519174b605e9669df082ae696a4e79214fa398eb
dekeffer <keffer@tutamail.com>
2026-03-17 14:09:06 +0100

    Bug Fix JS en CSS
    Ik heb wat bug gevonden in mijn code waardoor ik de code niet kon uitvoeren nu zijn deze als het goed is gefixed

--- html/js/rondetijd.js
+++ html/js/rondetijd.js
@@ -1,45 +1,48 @@
-let MM = document.getElementById('minutes'),
-SS = document.getElementById('seconds'),
-MS = document.getElementById('milliseconds');
+const knop = document.getElementById('toevoegknop');
+knop.addEventListener('click', () => {
+    let MM = document.getElementById('minutes'),
+    SS = document.getElementById('seconds'),
+    MS = document.getElementById('milliseconds');
+    console.log(MM)
+    Rondes = Rondes + 1;
 
-Rondes = Rondes + 1;
+    let Time = MM * 60 * 100 + SS * 100 + MS;
 
-let Time = MM * 60 * 100 + SS * 100 + MS;
+    if (Time > Fast){
+        Fast = Time
+        document.getElementById("SlowTime").innerHTML = MM, ":", SS, ",", MS;
+    }
 
-if (Time > Fast){
-    Fast = Time
-    document.getElementById("SlowTime").innerHTML = MM, ":", SS, ",", MS;
-}
+    let Ronde3 = Ronde2;
+    let Ronde2 = Ronde1;
+    let Ronde1 = Time;
 
-let Ronde3 = Ronde2;
-let Ronde2 = Ronde1;
-let Ronde1 = Time;
+    let MM3 = Math.round(Ronde3 / 100 / 60);
+    let SS3 = Math.round(Ronde3 -(MM3 * 60 * 100) / 100);
+    let MS3 = Math.round(Ronde3 -(SS3 * 100))
 
-let MM3 = Math.round(Ronde3 / 100 / 60);
-let SS3 = Math.round(Ronde3 -(MM3 * 60 * 100) / 100);
-let MS3 = Math.round(Ronde3 -(SS3 * 100))
+    let MM2 = Math.round(Ronde2 / 100 / 60);
+    let SS2 = Math.round(Ronde2 -(MM2 * 60 * 100) / 100);
+    let MS2 = Math.round(Ronde2 -(SS2 * 100))
 
-let MM2 = Math.round(Ronde2 / 100 / 60);
-let SS2 = Math.round(Ronde2 -(MM2 * 60 * 100) / 100);
-let MS2 = Math.round(Ronde2 -(SS2 * 100))
+    let MM1 = Math.round(Ronde1 / 100 / 60);
+    let SS1 = Math.round(Ronde1 -(MM1 * 60 * 100) / 100);
+    let MS1 = Math.round(Ronde1 -(SS1 * 100))
 
-let MM1 = Math.round(Ronde1 / 100 / 60);
-let SS1 = Math.round(Ronde1 -(MM1 * 60 * 100) / 100);
-let MS1 = Math.round(Ronde1 -(SS1 * 100))
+    document.getElementById("Ronde1").innerHTML = MM1, ":", SS1, ",", MS1;
+    document.getElementById("Ronde2").innerHTML = MM2, ":", SS2, ",", MS2;
+    document.getElementById("Ronde3").innerHTML = MM3, ":", SS3, ",", MS3;
 
-document.getElementById("Ronde1").innerHTML = MM1, ":", SS1, ",", MS1;
-document.getElementById("Ronde2").innerHTML = MM2, ":", SS2, ",", MS2;
-document.getElementById("Ronde3").innerHTML = MM3, ":", SS3, ",", MS3;
+    if (Time < Slow){
+        Slow = Time
+        document.getElementById("SlowTime").innerHTML = MM, ":", SS, ",", MS;
+    }
 
-if (Time < Slow){
-    Slow = Time
-    document.getElementById("SlowTime").innerHTML = MM, ":", SS, ",", MS;
-}
+    let Total = Total + Time;
 
-let Total = Total + Time;
+    let Gem = Total / Rondes;
 
-let Gem = Total / Rondes;
-
-let GemMM = Math.round(Gem / 100 / 60);
-let GemSS = Math.round(Gem -(GemMM * 60 * 100) / 100);
-let GemMS = Math.round(Gem -(GemSS * 100))
+    let GemMM = Math.round(Gem / 100 / 60);
+    let GemSS = Math.round(Gem -(GemMM * 60 * 100) / 100);
+    let GemMS = Math.round(Gem -(GemSS * 100));
+});
